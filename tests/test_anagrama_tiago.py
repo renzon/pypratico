ce667738ec9071e845ba340f2ba68d555286ea0d
mulhergorila <tiago@mulhergorila.com>
2017-05-26 00:21:25 -0300

    PEP 8

--- tests/test_anagrama_tiago.py
+++ tests/test_anagrama_tiago.py
@@ -1,7 +1,9 @@
 from exercicios.anagrama_tiago import eh_anagrama
 
+
 def test_eh_anagrama_tiago():
     assert eh_anagrama('ABA', 'BAA')
+
 
 def test_eh_anagrama_tiago_falso():
     assert not eh_anagrama('ABA', 'ACA')
