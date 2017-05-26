ca70cc68de37d3f3063ce89edb618f21658c43f5
mulhergorila <tiago@mulhergorila.com>
2017-05-26 18:57:38 -0300

    Tratando os espaços em branco das strings

--- exercicios/anagrama_tiago.py
+++ exercicios/anagrama_tiago.py
@@ -1,7 +1,10 @@
 def eh_anagrama(str1, str2):
-    anagrama = sorted(str1.lower()) == sorted(str2.lower())
+    str1 = str1.lower().replace(" ", "")
+    str2 = str2.lower().replace(" ", "")
+    anagrama = sorted(str1) == sorted(str2)
     return anagrama
 
 
 if __name__ == '__main__':
     print(eh_anagrama('ABA', 'AaB'))
+    print(eh_anagrama('THE ALIAS MEN', 'ALAN SMITHEE'))
--- tests/test_anagrama_tiago.py
+++ tests/test_anagrama_tiago.py
@@ -3,6 +3,7 @@ from exercicios.anagrama_tiago import eh_anagrama
 
 def test_eh_anagrama_tiago():
     assert eh_anagrama('ABA', 'BAA')
+    assert eh_anagrama('THE ALIAS MEN', 'ALAN SMITHEE')
 
 
 def test_eh_anagrama_tiago_falso():
