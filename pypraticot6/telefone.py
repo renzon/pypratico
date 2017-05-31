7bba66a3a5d50022aac523ef4e052fc272331a83
renzon <renzon@gmail.com>
2017-05-30 22:56:12 -0300

    Redicar implementado com variavel global
    Mas teve bug em producao, telefones diferentes sabem o ultimo numero discado

--- pypraticot6/telefone.py
+++ pypraticot6/telefone.py
@@ -1,3 +1,11 @@
+ultimo_numero = None
+
+
 class Telefone:
     def ligar(self, numero):
+        global ultimo_numero
+        ultimo_numero = numero
         return f'ligar para {numero}'
+
+    def rediscar(self):
+        return self.ligar(ultimo_numero)
--- tests/test_telefone.py
+++ tests/test_telefone.py
@@ -2,15 +2,30 @@ import pytest
 
 from pypraticot6.telefone import Telefone
 
+NUMEROS_VALIDOS = ['8765432', 2345678]
+
 
 def test_telefone_init():
     assert (Telefone() is not None)
 
 
-numeros_validos = ['8765432', 2345678]
-
-
-@pytest.mark.parametrize('numero', numeros_validos)
+@pytest.mark.parametrize('numero', NUMEROS_VALIDOS)
 def test_ligar(numero):
     telefone = Telefone()
     assert 'ligar para ' + str(numero) == telefone.ligar(numero)
+
+
+@pytest.mark.parametrize('numero', NUMEROS_VALIDOS)
+def test_rediscar(numero):
+    telefone = Telefone()
+    telefone.ligar(numero)
+    assert 'ligar para ' + str(numero) == telefone.rediscar()
+
+
+@pytest.mark.parametrize('numero', NUMEROS_VALIDOS)
+def test_rediscar_dois_telefone_diferentes(numero):
+    telefone = Telefone()
+    telefone.ligar(numero)
+    telefone_2 = Telefone()
+    outro_numero = str(numero) + '1'
+    assert 'ligar para ' + str(outro_numero) == telefone_2.rediscar()
